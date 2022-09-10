8a65176a135df044e04fe4df8fda8798e6005b96
JosephRidge <josephridge18@gmail.com>
2022-09-10 17:00:36 +0300

    Test one Check 😎

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WayOfTheJedi
+
+## credit_card_mask response : 
+![Screenshot at 2022-09-10 16-58-11](https://user-images.githubusercontent.com/42699812/189486618-09c02394-258a-42c2-93ac-bf77a70c7170.png)
+
+-------------------------------------------------------------
---
+++ credit_card_mask.py
@@ -0,0 +1,26 @@
+"""
+Usually when you buy something, 
+you're asked whether your credit card number,
+phone number or answer to your most secret 
+question is still correct. However, 
+since someone could look over your shoulder, 
+you don't want that shown on your screen. Instead, we mask it.
+"""
+import re
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
+def maskify(cc):
+    # confirm that the entry is more than four characters
+    # if more than four rall all first indexes after -4 with #
+    # Slice the string entry into a new string without the last four charcaters
+    print(cc)
+    x = cc[:-4]
+    print(x)
+    y = response = re.sub(r"[a-zA-Z0-9+.\s! # $ % &: ’()@' * + / = ? ^ _ ` { | } ~ -]","#",x)
+    print(y)
+    z =cc[-4:] 
+    return(y+z)
+
+#  input your card number / phone number / secret to phone
+credential = input("Input either your card number/ phone number/ most secret :  ")
+print(maskify(credential))
+
